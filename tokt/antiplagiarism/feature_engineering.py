54a3a0244e78005f276cf58f67c43435219b3a9c
Maxim Veremchuk <maxverem12398@gmail.com>
2020-11-14 22:33:36 +0200

    fix html generator

--- tokt/antiplagiarism/feature_engineering.py
+++ tokt/antiplagiarism/feature_engineering.py
@@ -1,26 +1,26 @@
 import pandas as pd
 import numpy as np
 import os
-import helpers 
+import helpers
 from sklearn.feature_extraction.text import CountVectorizer
 
 def numerical_dataframe(csv_file='data/file_information.csv'):
     df = pd.read_csv(csv_file)
     df.loc[:,'Class'] =  df.loc[:,'Category'].map({'non': 0, 'heavy': 1, 'light': 2, 'cut': 3, 'orig': -1})
     df.loc[:,'Category'] =  df.loc[:,'Category'].map({'non': 0, 'heavy': 1, 'light': 2, 'cut': 3, 'orig': -1})
-    
+
     return df
 
 def calculate_containment(df, n, answer_filename):
     answer_text, answer_task  = df[df.File == answer_filename][['Text', 'Task']].iloc[0]
     source_text = df[(df.Task == answer_task) & (df.Class == -1)]['Text'].iloc[0]
-    
+
     counter = CountVectorizer(analyzer='word', ngram_range=(n,n))
     ngrams_array = counter.fit_transform([answer_text, source_text]).toarray()
-    
+
     count_common_ngrams = sum(min(a, s) for a, s in zip(*ngrams_array))
     count_ngrams_a = ngrams_array[0].sum()
-    
+
     if(np.isnan(count_common_ngrams) or count_ngrams_a==0):
         return 0
     return count_common_ngrams / count_ngrams_a
@@ -28,10 +28,10 @@ def calculate_containment(df, n, answer_filename):
 def lcs_norm_word(answer_text, source_text):
     a_words = answer_text.split()
     s_words = source_text.split()
-    
+
     a_word_count = len(a_words)
     s_word_count = len(s_words)
-        
+
     lcs_matrix = np.zeros((s_word_count + 1, a_word_count + 1), dtype=int)
 
     for s, s_word in enumerate(s_words, 1):
@@ -40,17 +40,19 @@ def lcs_norm_word(answer_text, source_text):
                 lcs_matrix[s][a] = lcs_matrix[s-1][a-1] + 1
             else:
                 lcs_matrix[s][a] = max(lcs_matrix[s-1][a], lcs_matrix[s][a-1])
-    
+
     lcs = lcs_matrix[s_word_count][a_word_count]
-    
+
+    if(np.isnan(lcs) or a_word_count==0):
+        return 0
     return lcs / a_word_count
 
 def create_containment_features(df, n, column_name=None):
     containment_values = []
-    
+
     if(column_name==None):
         column_name = 'c_'+str(n) # c_1, c_2, .. c_n
-    
+
     for i in df.index:
         file = df.loc[i, 'File']
 
@@ -59,17 +61,17 @@ def create_containment_features(df, n, column_name=None):
             containment_values.append(c)
         else:
             containment_values.append(-1)
-    
+
     print(str(n)+'-gram containment features created!')
     return containment_values
 
 def create_lcs_features(df, column_name='lcs_word'):
-    
+
     lcs_values = []
-    
+
     for i in df.index:
         if df.loc[i,'Category'] > -1:
-            answer_text = df.loc[i, 'Text'] 
+            answer_text = df.loc[i, 'Text']
             task = df.loc[i, 'Task']
             orig_rows = df[(df['Class'] == -1)]
             orig_row = orig_rows[(orig_rows['Task'] == task)]
@@ -84,7 +86,7 @@ def create_lcs_features(df, column_name='lcs_word'):
     return lcs_values
 
 def train_test_data(complete_df, features_df, selected_features):
-    df = pd.concat([complete_df, features_df[selected_features]], axis=1)    
+    df = pd.concat([complete_df, features_df[selected_features]], axis=1)
     df_train = df[df.Datatype == 'train']
     df_test = df[df.Datatype == 'test']
 
@@ -93,15 +95,15 @@ def train_test_data(complete_df, features_df, selected_features):
 
     test_x = df_test[selected_features].values
     test_y = df_test['Class'].values
-    
+
     return (train_x, train_y), (test_x, test_y)
 
 def make_csv(x, y, filename, data_dir):
     if not os.path.exists(data_dir):
         os.makedirs(data_dir)
-    
+
     pd.concat([pd.DataFrame(y), pd.DataFrame(x)], axis=1).to_csv(os.path.join(data_dir, filename), header=False, index=False)
-    
+
     print('Path created: '+str(data_dir)+'/'+str(filename))
 
 def generate_features(df, ngram_range):
@@ -128,7 +130,7 @@ if __name__ == "__main__":
     transformed_df = numerical_dataframe(csv_file ='data/file_information.csv')
     text_df = helpers.create_text_column(transformed_df)
     print(text_df)
-    
+
     # sample_text = text_df.iloc[0]['Text']
     # print('Sample processed text:\n\n', sample_text)
 
--- tokt/antiplagiarism/generate_html.py
+++ tokt/antiplagiarism/generate_html.py
@@ -2,14 +2,34 @@ open_tag = """
 <!DOCTYPE html>
 <html>
 <head>
-<title>Plagiarizm report</title>
+<title>Plagiarism report</title>
 </head>
 <style>
-.tooltip {
-  position: relative;
-  display: inline-block;
-  font-family: Helvetica, sans-serif;
+
+body {
+    font-family: Helvetica, sans-serif;
+    background: #222;
+    margin: 0;
+    color: lightgray;
+    height:100%
 }
+
+.module-border-wrap {
+    padding: 1rem;
+    background: linear-gradient(to right, red, purple);
+    padding: 2px;
+}
+
+.module {
+  background: #222;
+  padding: 2rem;
+}
+
+.tooltip {
+    position: relative;
+    display: inline-block;
+}
+
 
 .tooltip .tooltiptext {
   visibility: hidden;
@@ -34,24 +54,53 @@ open_tag = """
   border-width: 5px;
   border-style: solid;
   border-color: transparent transparent black transparent;
+
 }
 
 .tooltip:hover .tooltiptext {
   visibility: visible;
 }
+
+.paragraph {
+    border-radius: 10px;
+    text-indent: 40px;
+    padding: 3px;
+}
+
+hr {
+    border: none;
+    color: purple;
+    background-color: purple;
+    height: 2px;
+    width: 400px;
+}
+
 </style>
-<body">
+<body>
+    <div class="module-border-wrap">
+        <div class="module">
+            <h1 style="text-align:center">Plagiarism report</h1>
+            <hr>
 """
 
 parahraph_tag = """
-<div class="tooltip">
-    <p style = "background-color:{0}; text-indent: 40px;">{1}</p>
-    <span class="tooltiptext">{2}</span>
+<div class="tooltip ">
+        <p class="paragraph" style = "background:rgba({0}, .4); border: 2px solid rgba({0}, 1); ">{1}</p>
+        <span class="tooltiptext">{2}</span>
 </div>
 <br>
 """
 
+end_report = """
+<hr>
+<div>
+    <h3 style = "text-indent: 40px;">Document originality average: {0}%</h3>
+</div>
+"""
+
 close_tag = """
+        </div>
+    </div>
 </body>
 </html> """
 
@@ -59,16 +108,19 @@ close_tag = """
 def generate_html(paragraphs):
     html_content = ""
     html_content += open_tag
-    plagiat_type = {0: "Non pagiat", 1: "Heavily paraphrased",
+    plagiat_type = {0: "Non plagiat", 1: "Heavily paraphrased",
                     2: "Lightly paraphrased", 3: "Copypasted"}
+    all_percents = []
     for paragraph in paragraphs:
         percents = int(paragraph['prob'][0] * 100)
+        all_percents.append(percents)
         percent_text = "Original percent: {0}%<br>Task type: {1}<br>Plagiat type: {2}".format(
             percents, paragraph['task'], plagiat_type[paragraph['class']])
-        color = '#%02x%02x%02x' % (
+        color = '%02d,%02d,%02d' % (
             int((1 - paragraph['prob'][0]) * 256), int(paragraph['prob'][0] * 256), 0)
         html_content += parahraph_tag.format(
             color, paragraph['text'], percent_text)
+    html_content += end_report.format(int(sum(all_percents) / len(all_percents)))
     html_content += close_tag
 
     f = open("plagiat_report.html", "w")
