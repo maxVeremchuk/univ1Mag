8a715e1d43bc5c9f333c3981ee8304eed49eff69
Maxim Veremchuk <maxverem12398@gmail.com>
2021-01-28 22:35:28 +0200

    Init coursework

---
+++ coursework/utils.py
@@ -0,0 +1,50 @@
+import nltk
+import pickle
+import re
+import numpy as np
+
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+
+# Paths for all resources for the bot.
+RESOURCE_PATH = {
+    'INTENT_RECOGNIZER': 'intent_recognizer.pkl',
+    'TAG_CLASSIFIER': 'tag_classifier.pkl',
+    'TFIDF_VECTORIZER': 'tfidf_vectorizer.pkl',
+    'THREAD_EMBEDDINGS_FOLDER': 'thread_embeddings_by_tags',
+    'WORD_EMBEDDINGS': 'data/word_embeddings.tsv',
+}
+
+
+def text_prepare(text):
+    """Performs tokenization and simple preprocessing."""
+    replace_by_space_re = re.compile('[/(){}\[\]\|@,;]')
+    good_symbols_re = re.compile('[^0-9a-z #+_]')
+    stopwords_set = set(stopwords.words('english'))
+
+    text = text.lower()
+    text = replace_by_space_re.sub(' ', text)
+    text = good_symbols_re.sub('', text)
+    text = ' '.join([x for x in text.split() if x and x not in stopwords_set])
+
+    return text.strip()
+
+def question_to_vec(question, embeddings, dim):
+    """Transforms a string to an embedding by averaging word embeddings."""
+    vector = np.zeros(dim)
+    count = 0
+    words = question.split()
+    if words:
+        for word in words:
+            if word in embeddings:
+                    vector += embeddings[word][:300]
+                    count += 1
+    if(count!=0):
+        vector = vector / count
+    return vector
+
+
+def unpickle_file(filename):
+    """Returns the result of unpickling the file content."""
+    with open(filename, 'rb') as f:
+        return pickle.load(f)
